356c7676b958636a36e5d9dedfd4e7bd661c494a
Selech <selech@live.dk>
2015-02-13 12:07:42 +0100

    Revert "Revert "First draft""
    This reverts commit d372abb9f5862b7dee550859a0a5b0dffc59bab3.

---
+++ WindowsFormsApplication1/ClassLibrary1/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace Domain
+{
+    public class Customer
+    {
+        public string Address { set; get; }
+        public int Phone { set; get; }
+    }
+}
